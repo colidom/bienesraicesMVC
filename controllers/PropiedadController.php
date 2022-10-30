f98b75b42da688440b0af3c74743acfde15cd742
colidom <colidom@outlook.com>
2022-10-30 01:00:20 +0000

    Asociando URL's a métodos del Controlador

---
+++ controllers/PropiedadController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Controllers;
+
+class PropiedadController {
+
+    public static function index() {
+        echo "Index Controller...";
+    }
+
+    public static function crear() {
+        echo "Crear Controller...";
+    }
+
+    public static function actualizar() {
+        echo "Actualizar Controller...";
+    }
+}
