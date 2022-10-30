fb1857d1793a89a3f131e1dea931cd12897fad10
colidom <colidom@outlook.com>
2022-10-30 01:47:05 +0100

    Registrando URL's y los métodos asociados a ellas

--- public/index.php
+++ public/index.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../includes/app.php';
 use MVC\Router;
 
 $router = new Router;
+
+$router->get('/nosotros', 'funcion_nosotros');
+$router->get('/admin', 'funcion_admin');
+$router->comprobarRutas();
--- router.php
+++ router.php
@@ -4,7 +4,26 @@ namespace MVC;
 
 class Router {
 
-    public function __construct () {
-        echo "Creando el Router";
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn) {
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas() {
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+
+        if ($metodo === 'GET') {
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if ($fn) {
+            // La función existe
+            call_user_func($fn, $this);
+        } else {
+            echo "Error 404: Página no encontrada";
+        }
     }
 }
