95d35d29f0527189ed77cded25370a75367830db
colidom <colidom@outlook.com>
2022-11-05 01:08:08 +0000

    Fixed: Error que impedía se viera bien el formulario de crear propiedad

--- views/propiedades/crear.php
+++ views/propiedades/crear.php
@@ -1,7 +1,7 @@
 <main class="contenedor seccion">
     <h1>Crear</h1>
 
-    <form classs="formulario" method="POST">
+    <form class="formulario" method="POST">
         <?php include __DIR__ . '/formulario.php'; ?>
 
         <input type="submit" value="Crear Propiedad" class="boton boton-verde">
