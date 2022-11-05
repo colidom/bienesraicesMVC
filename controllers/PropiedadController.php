cb4a2df9cc99805507ba7c6a4bff02b7fb10a97a
colidom <colidom@outlook.com>
2022-11-05 01:29:54 +0000

    Trabajando con las peticiones POST

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -23,6 +23,10 @@ class PropiedadController {
         $propiedad = new Propiedad;
         $vendedores = Vendedor::all();
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            
+        }
+
         $router->render('/propiedades/crear', [
             'propiedad' => $propiedad,
             'vendedores' => $vendedores
