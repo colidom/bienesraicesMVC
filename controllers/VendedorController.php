7fbec8cce64f75b92801ae28480282841caf73ef
colidom <colidom@outlook.com>
2022-11-06 21:12:57 +0000

    Creando el Controlador para Vendedores

---
+++ controllers/VendedorController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+use Model\Propiedad;
+use Model\Vendedor;
+
+class VendedorController {
+
+    public static function crear() {
+        echo "Crear vendedor";
+    }
+
+    public static function actualizar() {
+        echo "Actualizar vendedor";
+    }
+
+    public static function eliminar() {
+        echo "Eliminar vendedor";
+    }
+}
