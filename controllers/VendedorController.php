e596369a6df5d56f4d865c2da3526eccee15b809
colidom <colidom@outlook.com>
2022-11-07 01:02:51 +0000

    Validación de Vendedores

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -12,6 +12,20 @@ class VendedorController {
         $vendedor = new Vendedor;
         $errores = Vendedor::getErrores();
 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            // Crea una nueva instancia
+            $vendedor = new Vendedor($_POST['vendedor']);
+
+            // Validar
+            $errores = $vendedor->validar();
+
+            if (empty($errores)) {
+                // Guarda en la base de datos
+                $vendedor->guardar();
+            }
+        }
+
         $router->render('/vendedores/crear', [
             'vendedor' => $vendedor,
             'errores' => $errores
