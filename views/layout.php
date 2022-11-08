6f86de79de1d221a2a3ffc78cd96f714645d6c98
colidom <colidom@outlook.com>
2022-11-08 00:10:51 +0000

    Correcciones de rutas

--- views/layout.php
+++ views/layout.php
@@ -33,15 +33,15 @@
                <div class="derecha">
                 <img class="dark-mode-boton" src="../build/img/dark-mode.svg">
                     <div class="navegacion">
-                        <a href="nosotros.php">Nosotros</a>
-                        <a href="anuncios.php">Anuncios</a>
-                        <a href="blog.php">Blog</a>
-                        <a href="contacto.php">Contacto</a>
+                        <a href="/nosotros">Nosotros</a>
+                        <a href="/anuncios">Anuncios</a>
+                        <a href="/blog">Blog</a>
+                        <a href="/contacto">Contacto</a>
                         <?php if (!$auth): ?>
-                            <a href="login.php">Iniciar Sesión</a>
+                            <a href="/login">Iniciar Sesión</a>
                         <?php endif; ?>
                         <?php if ($auth): ?>
-                            <a href="cerrar-sesion.php">Cerrar Sesión</a>
+                            <a href="/cerrar-sesion">Cerrar Sesión</a>
                         <?php endif; ?>
                     </div>
                </div>
