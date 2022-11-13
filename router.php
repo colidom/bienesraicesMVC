f2a432c393023389c397f41cfe530e9b1d27010a
colidom <colidom@outlook.com>
2022-11-13 22:55:41 +0000

    Proteger las Rutas

--- router.php
+++ router.php
@@ -20,6 +20,20 @@ class Router
 
     public function comprobarRutas()
     {
+        session_start();
+        $auth = $_SESSION['login'] ?? null;
+
+        // Array de rutas protegidas
+        $rutas_protegidas = [
+            '/admin',
+            '/propiedades/crear',
+            '/propiedades/actualizar',
+            '/propiedades/eliminar',
+            '/vendedores/crear',
+            '/vendedores/actualizar',
+            '/vendedores/eliminar',
+        ];
+
         $urlActual = $_SERVER['PATH_INFO'] ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
 
@@ -27,6 +41,11 @@ class Router
             $fn = $this->rutasGET[$urlActual] ?? null;
         } else {
             $fn = $this->rutasPOST[$urlActual] ?? null;
+        }
+
+        // Proteger las rutas
+        if(in_array($urlActual, $rutas_protegidas) && !$auth) {
+            header('Location: /');
         }
 
         if ($fn) {
